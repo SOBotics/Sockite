fa2c8a5ed664c8ea7d6bd28f3ce7a1ac1a1f5551
paper1111 <hilarylau12342@gmail.com>
2018-01-18 21:09:13 +0800

    add checkuser json parse

--- Sources/sockite/Checkuser.swift
+++ Sources/sockite/Checkuser.swift
@@ -2,10 +2,10 @@ import Foundation
 
 class Checkuser: Command {
     init() {
-        super.init(syntax: ["checkuser"], args: 2, exec: { msg, args in
+        super.init(syntax: ["checkuser"], args: 3, exec: { msg, args in
             let session = URLSession(configuration: .default)
             dataTask?.cancel()
-            if var urlComponents = URLComponents(string: "https://api.stackexchange.com/2.2/users/\(args)/questions") {
+            if var urlComponents = URLComponents(string: "https://api.stackexchange.com/2.2/users/\(args[1]);\(args[2])/questions") {
                 urlComponents.query = "pagesize=100&order=desc&sort=activity&site=stackoverflow&filter=!9YdnSEcyO&key=OJ*iP6ih)G0W1CQFgKllSg(("
                 guard let url = urlComponents.url else {
                     return
@@ -16,16 +16,30 @@ class Checkuser: Command {
                     defer { dataTask = nil }
                     let ponse = response as! HTTPURLResponse
                     print(ponse.statusCode)
+                    var resToChat = ""
                     if let error = error {
                         print(error.localizedDescription)
                     } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
                         do {
                             let json = try JSONDecoder().decode(CheckuserJSON.self, from: data)
+                            for item in json.items {
+                                for answer in item.answers {
+                                    if answer.owner.user_id != item.owner.user_id {
+                                        if String(answer.owner.user_id) == args[1] || String(answer.owner.user_id) == args[2] {
+                                            if resToChat == "" {
+                                                resToChat.append("Questions answered by target user:")
+                                            }
+                                            resToChat.append("\nQ: \(item.link); A: https://stackoverflow.com/a/\(String(answer.answer_id))")
+                                        }
+                                    }
+                                }
+                            }
+                            room.postMessage(resToChat)
                         } catch let jsonError {
-                            print(jsonError.localizedDescription)
+                            print("JSON error occured: `\(jsonError.localizedDescription)` cc @paper")
                         }
                     } else {
-                        print("unknown error")
+                        room.postMessage("Unknown error at `Checkuser.swift#L41` cc @paper")
                     }
                 }
                 dataTask?.resume()
