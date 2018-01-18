c1f55fb69b455035c148f5fd23e1b9f6fe252360
paper1111 <hilarylau12342@gmail.com>
2018-01-18 21:38:10 +0800

    bug fix

--- Sources/sockite/Checkuser.swift
+++ Sources/sockite/Checkuser.swift
@@ -22,21 +22,26 @@ class Checkuser: Command {
                     } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
                         do {
                             let json = try JSONDecoder().decode(CheckuserJSON.self, from: data)
-                            for item in json.items {
-                                for answer in item.answers {
-                                    if answer.owner.user_id != item.owner.user_id {
-                                        if String(answer.owner.user_id) == args[1] || String(answer.owner.user_id) == args[2] {
-                                            if resToChat == "" {
-                                                resToChat.append("Questions answered by target user:")
+                            guard let items = json.items else {
+                                return
+                            }
+                            for item in items {
+                                if let answers = item.answers {
+                                    for answer in answers {
+                                        if answer.owner!.user_id! != item.owner!.user_id! {
+                                            if String(answer.owner!.user_id!) == args[1] || String(answer.owner!.user_id!) == args[2] {
+                                                if resToChat == "" {
+                                                    resToChat.append("Questions answered by target user:")
+                                                }
+                                                resToChat.append("\nQ: \(item.link!); A: https://stackoverflow.com/a/\(String(answer.answer_id!))")
                                             }
-                                            resToChat.append("\nQ: \(item.link); A: https://stackoverflow.com/a/\(String(answer.answer_id))")
                                         }
                                     }
                                 }
                             }
                             room.postMessage(resToChat)
                         } catch let jsonError {
-                            room.postMessage("JSON error occured: `\(jsonError.localizedDescription)` cc @paper")
+                            room.postMessage("Error occured: `\(jsonError.localizedDescription)` cc @paper")
                         }
                     } else {
                         room.postMessage("Unknown error at `Checkuser.swift#L41` cc @paper")
--- Sources/sockite/decodables.swift
+++ Sources/sockite/decodables.swift
@@ -1,24 +1,24 @@
 struct CheckuserJSON: Decodable {
     struct Owner: Decodable {
-        var reputation: Int
-        var user_id: Int
-        var display_name: String
-        var link: String
+        var reputation: Int?
+        var user_id: Int?
+        var display_name: String?
+        var link: String?
     }
     struct Item: Decodable {
         struct Answer: Decodable {
-            var owner: Owner
-            var is_accepted: Bool
-            var answer_id: Int
+            var owner: Owner?
+            var is_accepted: Bool?
+            var answer_id: Int?
         }
-        var tags: [String]
-        var answers: [Answer]
-        var owner: Owner
-        var is_answered: Bool
-        var accepted_answer_id: Int
-        var question_id: Int
-        var link: String
-        var title: String
+        var tags: [String]?
+        var answers: [Answer]?
+        var owner: Owner?
+        var is_answered: Bool?
+        var accepted_answer_id: Int?
+        var question_id: Int?
+        var link: String?
+        var title: String?
     }
-    var items: [Item]
+    var items: [Item]?
 }
