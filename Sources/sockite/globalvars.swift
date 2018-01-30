92fdec1cd1456cbe910d53fdf6ad20a8185002ee
paper1111 <hilarylau12342@gmail.com>
2018-01-30 20:49:05 +0800

    push say

--- Sources/sockite/globalvars.swift
+++ Sources/sockite/globalvars.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 // array of commands
-let commands: [Command] = [Alive(), Checkuser()]
+let commands: [Command] = [Alive(), Checkuser(), Say()]
 
 var dataTask: URLSessionDataTask?
